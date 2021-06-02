23bc814efcf559a789cbf2d3e71199767e7708c4
Thng Yu Xuan <thngyuxuan@gmail.com>
2021-06-02 21:03:26 +0800

    Added About and Contact pagge

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Pages/Home'; 
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
@@ -11,7 +14,10 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path='/about' exact component={About} />
+          <Route path='/contact' exact component={Contact} />
         </Switch>
+        <Footer />
       </Router> 
     </>
   );
