edb8098f30830e411ef46ef031c04ca2ce71fba3
yeowww <ceyskiii@gmail.com>
2021-06-02 21:11:29 +0800

    fixed merge conflict

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Router } from 'react-router'
 import '../../App.css'
-import HeroSection from '../HeroSection'
+import HeroSection from '../HeroSection/HeroSection'
 import Features from './Features'
 
 function Home() {
