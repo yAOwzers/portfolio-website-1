2aa8ea0e032995fd8f47097e1840096a4dd8379e
Thng Yu Xuan <thngyuxuan@gmail.com>
2021-06-03 15:25:43 +0800

    Added footer animations

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,13 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
 //import { Router } from 'react-router'
-import '../../App.css'
-import HeroSection from '../HeroSection'
-=======
-import { Router } from 'react-router';
 import '../../App.css';
 import HeroSection from '../HeroSection/HeroSection';
->>>>>>> 623d790f9138a07ff9bf290507506cdf1670e550
+
 
 function Home () {
     return (
