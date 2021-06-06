6f218150a7d3a22fa9975529527ac3b0bac63253
yeowww <ceyskiii@gmail.com>
2021-06-06 19:20:43 +0800

    styled, functionable homepage

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Router } from 'react-router'
-import '../../App.css'
-import HeroSection from '../HeroSection/HeroSection'
-import Features from './Features'
+import { Router } from 'react-router';
+import '../../App.css';
+import HeroSection from '../HeroSection/HeroSection';
+import Features from './Features';
 
 function Home() {
     return (
